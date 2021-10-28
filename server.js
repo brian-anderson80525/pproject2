959d07ac2d8beed4d6f34d25f59b5f73ff8aa3b9
Brian Anderson <briananderson@MacBook-Pro-2.local>
2021-10-28 13:18:55 -0600

    commit 3 of build more setup done

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
-// import deps
-require("dotenv").config() // brings in .env vars
-const express = require("express") // web framework
-// const morgan = require("morgan") // logger
-// const methodOverride = require("method-override") // to swap request methods
-// const path = require("path") // helper functions for file paths
+///////////////////////////////////
+// Import our Dependencies
+///////////////////////////////////
+
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const path = require("path")
+const liquid = require("liquid-express-views")
 // const FruitsRouter = require("./controllers/fruit")
-// const UserRouter = require("./controllers/user")
-// const session = require("express-session") // session middleware
-// const MongoStore = require("connect-mongo") // save sessions in mongo
 
 
-// app object
-const app = express()
 
 
-// route
+
+/////////////////////////////////
+// Create our app with object, configure liquid
+/////////////////////////////////
+const viewsFolder = path.resolve(__dirname, "views/")
+const app = liquid(express(), {root: viewsFolder})
+
+
+
+/////////////////////////////////////////////
+// Register Our Middleware
+/////////////////////////////////////////////
+app.use(morgan("tiny"))
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+
+////////////////////////////////////////
+// Routes
+/////////////////////////////////////////
 app.get("/", (req, res) => {
     res.send("This App is working")
 })
 
-// listener
+
+
+
+/////////////////////////////////////////////
+// Setup Server Listener
+/////////////////////////////////////////////
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`listening on port ${PORT}`))
